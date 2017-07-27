eaaf7097af548221eff9bb6b88d7daebaff1929c
Tyler Thomson <tylerthomson@protonmail.com>
2017-07-27 10:11:08 -0500

    Adding unfinished weather forecast assignment

---
+++ tyler_thomson/weatherForecast/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  links: any[] = ['Burbank', 'Chicago', 'Dallas', 'DC', 'San Jose', 'Seattle' ]
+}
